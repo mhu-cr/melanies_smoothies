d1325faeaf32d12cf723960d4c98134bfbc1ca14
mhu-cr <mhu@cleverreach.com>
2026-02-19 13:34:20 +0100

    Import col function from snowflake.snowpark

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-
+from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
   """Choose the fruits you want in your cusotm smoothie!
   """
 )
-from snowflake.snowpark.functions import col
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
