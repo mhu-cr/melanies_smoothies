7508623640953411fa2ea945047b15f25f8634e5
mhu-cr <mhu@cleverreach.com>
2026-02-19 13:51:18 +0100

    Refactor session management for Snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,9 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
 
-#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -43,5 +45,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
       
-cnx = st.connection("snowflake")
-session = cnx.session()
+
